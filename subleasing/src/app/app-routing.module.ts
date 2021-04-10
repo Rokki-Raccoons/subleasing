045b51088e21b0bbf03cad262045db4570396d73
KolbyFlipper <43758037+KolbyFlipper@users.noreply.github.com>
2021-04-10 16:22:14 -0400

    add routing ts file

--- subleasing/src/app/app-routing.module.ts
+++ subleasing/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {EditListingComponent} from './edit-listing/edit-listing.component';
+import {ListingCardComponent} from './listing-card/listing-card.component';
+import {MessageBoxComponent} from './message-box/message-box.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header-component', component: HeaderComponent},
+  {path: 'edit-listing-component', component: EditListingComponent},
+  {path: 'listing-card-component', component: ListingCardComponent},
+  {path: 'message-box-component', component: MessageBoxComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
