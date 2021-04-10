36ce57a629cabafe88c4354fe22597cd045ae897
KolbyFlipper <43758037+KolbyFlipper@users.noreply.github.com>
2021-04-10 16:26:57 -0400

    update message box component for dyn routing

--- subleasing/src/app/message-box/message-box.component.ts
+++ subleasing/src/app/message-box/message-box.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import {MessageModel} from './message-model'
 
 @Component({
@@ -9,25 +9,28 @@ import {MessageModel} from './message-model'
 })
 export class MessageBoxComponent implements OnInit {
 
-  @Input() public message : MessageModel;
+  // @Input() public message : MessageModel;
 
   senderName = "None";
   senderID = -1;
   messageBody = "-";
   messageID = -1;
 
-  constructor() { 
-    this.message = {senderName: "-",
-                    senderID: 0,
-                    messageBody: "-",
-                    messageID: 0};
-  }
+  constructor(private route: ActivatedRoute) {}
+  //   this.message = {senderName: "-",
+  //                   senderID: 0,
+  //                   messageBody: "-",
+  //                   messageID: 0};
+  // }
 
   ngOnInit(): void {
-    this.senderName = this.message.senderName;
-    this.senderID = this.message.senderID;
-    this.messageBody = this.message.messageBody;
-    this.messageID = this.message.messageID;
+    this.route.queryParams.subscribe(params=>{
+    this.senderName = params.senderName;
+    this.senderID = params.senderID;
+    this.messageBody = params.messageBody;
+    this.messageID = params.messageID;
+  });
+
   }
 
 }
