2a906256293b690e13a7882d21761fbeb2adf1fe
KolbyFlipper <43758037+KolbyFlipper@users.noreply.github.com>
2021-04-26 18:19:45 -0400

    Add files via upload

--- subleasing/src/app/renter-view/renter-view.component.ts
+++ subleasing/src/app/renter-view/renter-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MessageBoxComponent} from '../message-box/message-box.component'
 import {EditListingComponent} from '../edit-listing/edit-listing.component'
 import {HeaderComponent} from '../header/header.component'
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-renter-view',
@@ -9,10 +10,14 @@ import {HeaderComponent} from '../header/header.component'
   styleUrls: ['./renter-view.component.css']
 })
 export class RenterViewComponent implements OnInit {
-
+  msg = new FormControl('');
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveMsg(){
+    
   }
 
 }
